98cdf1837f8ab8ada7661f7fc75ead77ece58f16
Kshitij Mani <mani.smhs@gmail.com>
2020-09-07 18:13:41 +0530

    Added processing for multiple spreadsheet

--- Scrapy_Project/amazonreview/amazonreview/config/spreadsheet.py
+++ Scrapy_Project/amazonreview/amazonreview/config/spreadsheet.py
@@ -5,28 +5,39 @@ class AmazonLinks:
 
     def __init__(self):
         self.client = self.create_client()
-        self.values = self.get_all_values_from_sheet()
-        self.data = self.construct_dict_from_values()
+        self.data = self.get_all_values_from_sheet()
+        print(self.values)
 
     def create_client(self):
-        return gspread.service_account('/Users/ashishbhatt/Downloads/AmazonLink-e2dbc198a2b4.json')
+        return gspread.service_account('../../AmazonLink-e2dbc198a2b4.json')
 
+    """ gets all data from all spread sheet and puts it in a dictionary"""
     def get_all_values_from_sheet(self):
-        sheet = self.client.open('AmazonLinks').sheet1
-        value = sheet.get_all_values()
-        return value
+        value = []
+        titles_list = []
+        data_dict = {}
+        spreadsheets = self.client.open('AmazonLinks')
+        # get list of all spreadsheet
+        for spreadsheet in spreadsheets:
+            titles_list.append(spreadsheet.title)
+        # fetch data from all the spreadsheed
+        for brand in titles_list:
+            sheet = spreadsheets.worksheet(brand)
+            value.extend(sheet.get_all_values()[:])
+            data_dict = self.construct_dict_from_values(value, data_dict)
+            value = []
+        return data_dict
 
-    def construct_dict_from_values(self):
-        dict_data = {}
-        headers = self.values.pop(0)
-        print(self.values)
-        data = []
-        for index in range(len(headers)):
-            for val in self.values:
-                data.append(val[index])
-            dict_data.setdefault(headers[index], data)
-            data = []
-        return dict_data
+    """Inputs value list from a spread sheet and appends to the dictionary passed"""
+    def construct_dict_from_values(self, values, dict):
+        headers = values.pop(0)
+        for value in values:
+            for index in range(len(headers)):
+                if headers[index] not in dict.keys():
+                    dict[headers[index]] = [value[index]]
+                else:
+                    dict[headers[index]].append(value[index])
+        return dict
 
 
 if __name__ == '__main__':
