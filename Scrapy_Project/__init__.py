24a6123e0b5e56fa4a22a496faed9cb3540de994
AshishBhatt14 <ashish.bhatt@onedirect.in>
2020-09-14 18:29:00 +0530

    iuaebfiuhauidfb

--- Scrapy_Project/__init__.py
+++ Scrapy_Project/__init__.py
@@ -1,6 +1,6 @@
-from Scrapy_Project.amazonreview.config.sheettosql import SheetToSql
-from Scrapy_Project.amazonreview.config.spreadsheet import AmazonLinks
-from Scrapy_Project.amazonreview.utils.mysqlutils import MysqlUtil
+from Scrapy_Project.amazonreview.amazon_review.amazon_review.onedirect_config import SheetToSql
+from Scrapy_Project.amazonreview.amazon_review.amazon_review.onedirect_config import AmazonLinks
+from Scrapy_Project.amazonreview.amazon_review.amazon_review.onedirect_config import MysqlUtil
 
 if __name__ == '__main__':
     SheetToSql(MysqlUtil(), AmazonLinks().data)
---
+++ Scrapy_Project/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+setup(
+    name='Scrapy_Project',
+    packages=find_packages(),
+)
