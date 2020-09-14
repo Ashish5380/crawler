a480dc1ff35cd3820bb27267ca621f6be8718e79
Kshitij Mani <mani.smhs@gmail.com>
2020-09-14 17:08:46 +0530

    added logs

--- Scrapy_Project/amazonreview/config/reviewurls.py
+++ Scrapy_Project/amazonreview/config/reviewurls.py
@@ -1,4 +1,5 @@
 from Scrapy_Project.amazonreview.models.amazon_data import AmazonData
+from Scrapy_Project.amazonreview.utils.logger import logger
 
 from sqlalchemy.orm import sessionmaker
 
@@ -8,7 +9,10 @@ class ReviewUrls:
     def read_all_urls(self):
         urls = []
         session = sessionmaker(bind=self.db_engine)
-        records = session().query(AmazonData).all()
+        try:
+            records = session().query(AmazonData).all()
+        except Exception as ex:
+            logger.error("Error reading Amazon data records from DB")
         for row in records:
             urls.append(row.product_link)
         return urls
