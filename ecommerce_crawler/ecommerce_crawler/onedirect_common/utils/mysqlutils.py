a9e7881c4056d9ffb87a3a962dda1faf7db2dca8
Ashish5380 <ashishbhatt5380@gmail.com>
2020-10-06 22:38:42 +0530

    Update mysqlutils.py
    change for making it public

--- ecommerce_crawler/ecommerce_crawler/onedirect_common/utils/mysqlutils.py
+++ ecommerce_crawler/ecommerce_crawler/onedirect_common/utils/mysqlutils.py
@@ -12,9 +12,9 @@ class MysqlUtil:
 
     @classmethod
     def intialize_db_config_map(cls):
-        cls.db_config_map.__setitem__('USER', 'amazonuser')
-        cls.db_config_map.__setitem__('PASS', 'Am@z0nU&3r')
-        cls.db_config_map.__setitem__('HOST', '172.16.1.105')
+        cls.db_config_map.__setitem__('USER', "<Your user name>")
+        cls.db_config_map.__setitem__('PASS', '<Your password>')
+        cls.db_config_map.__setitem__('HOST', '<host IP>')
         cls.db_config_map.__setitem__('PORT', '3306')
         cls.db_config_map.__setitem__('DB', 'amazon')
 
